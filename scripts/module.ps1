6465d8f8e1f44d19d86c02ce657dee948430d962
Louis Ruocco <louisruocco1@gmail.com>
2024-06-12 21:26:50 +0100

    made good head way with the update learning topic function

--- scripts/module.ps1
+++ scripts/module.ps1
@@ -1,12 +1,35 @@
-Function Azure-Login {
+Function Check-AzureSession {
     $context = Get-AzContext
     if(!($context)){
         Connect-AzAccount
     }
 }
 
-function Update-OtherLearning {
-    Azure-Login
+function Update-LearningTopic {
+    Check-AzureSession
+    $kvparams = @{
+        VaultName = "nonna-kv"
+        Name = "token"
+    }
+
+    $sacontextparams = @{
+        StorageAccountName = "nonnastoracc"
+        SasToken = Get-AzKeyVaultSecret @kvparams -AsPlainText
+    }
+
+    $sacontext = New-AzStorageContext @sacontextparams
+
+    $blobcontentparams = @{
+        Context = $sacontext
+        Container = "nonnacontainer"
+        Blob = "other learning.txt"
+        Destination = "C:\Nonna\Learning"
+    }
+    Write-output "Downloading other learning text document for editing"
+    Get-AzStorageBlobContent @blobcontentparams
+    cls
+    $value = Read-Host -Prompt "Update Learning Topic Here"
+    $value
 }
 
-Update-OtherLearning
+Update-LearningTopic
